b163abf4642ad7d502cef8bf4bf8134afe3cfd09
huynhquangtiep <tieplangtom@gmail.com>
2023-10-15 03:37:00 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "expert",
-};
-
-module.exports = nextConfig
+module.exports = {
+    output: {
+      export: true,
+    },
+  };
