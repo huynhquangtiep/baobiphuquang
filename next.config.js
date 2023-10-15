1b0c7517491c47f0fa135b329115cf5e30934913
huynhquangtiep <tieplangtom@gmail.com>
2023-10-15 16:34:18 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      erverComponentsExternalPackages: ["mongoose"],
-    },
   };
   
   module.exports = nextConfig;
