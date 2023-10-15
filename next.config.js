ffdede2b0aebe60cd7f4f2783bbb36647e2c51ac
huynhquangtiep <tieplangtom@gmail.com>
2023-10-15 11:02:01 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    output : "standalone",
-}
-
-module.exports = nextConfig
+    output: 'export',
+    // Các cấu hình khác của bạn...
+  };
+  
+  module.exports = nextConfig;
