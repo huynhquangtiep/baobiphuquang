a8ffe81e93305601ad7baa22734aec4970dd6455
huynhquangtiep <tieplangtom@gmail.com>
2023-10-15 18:26:43 +0700

    first commit

--- src/utils/index.js
+++ src/utils/index.js
@@ -184,18 +184,17 @@ export const AvailableSizes = [
   },
 ];
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyAEPwpouC2JuZgqGNeq8J0K1sXWW5FG-ds",
-  authDomain: "baobiphuquang-93631.firebaseapp.com",
-  projectId: "baobiphuquang-93631",
-  storageBucket: "baobiphuquang-93631.appspot.com",
-  messagingSenderId: "208650888048",
-  appId: "1:208650888048:web:b46c366bfe0264bdee65b5",
-  measurementId: "G-3EVNE7JLPD",
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-export const firebaseStroageURL =
-  "gs://baobiphuquang-93631.appspot.com";
+export const firebaseStroageURL = process.env.REACT_APP_FIREBASE_STORAGE_URL;
 
 export const addNewAddressFormControls = [
   {
